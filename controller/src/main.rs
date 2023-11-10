df867ef64b54ddacf57f481d8b77d26f2de7d866
Ella Hinschen <ella@ellahin.com>
2023-11-10 19:14:13 +1100

    changed pwm to rppal

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,3 +1,33 @@
+use common_data::commands::movement::Movement;
+use std::io::{BufReader, BufWriter, Read, Write};
+use std::net::TcpStream;
 fn main() {
-    println!("{}", "Hello world")
+    let mut stream_raw = TcpStream::connect("127.0.0.1:3000").unwrap();
+    let mut stream = BufWriter::new(stream_raw.try_clone().unwrap());
+
+    let mut command = Movement::new();
+
+    command.set_yaw(-100).expect("invalid yaw input");
+
+    command.set_acceleration(100).expect("invalid accel input");
+
+    let movement_buff = command.generate_packet();
+
+    let mut buff_end: Vec<u8> = [255].to_vec();
+
+    let mut buff: Vec<u8> = movement_buff.to_vec();
+
+    buff.append(&mut buff_end);
+
+    stream.write_all(&buff).expect("Failed to write to server");
+
+    stream.flush().unwrap();
+
+    let mut reader = BufReader::new(stream_raw);
+
+    let mut buff: Vec<u8> = Vec::new();
+
+    reader.read_to_end(&mut buff).unwrap();
+
+    println!("buff: {:#?}", buff);
 }
--- sub/src/repo/physical/motors/servosixfront.rs
+++ sub/src/repo/physical/motors/servosixfront.rs
@@ -1,179 +1,294 @@
 use crate::repo::physical::motors::base::Motor;
 use common_data::libs::math::maprang::map_range;
 
-use wiringpi::pin::{Gpio, SoftPwmPin};
+use rppal::gpio::{Gpio, OutputPin};
+use std::time::Duration;
+
+const PERIOD_MS: u64 = 20;
+const PULSE_MIN_US: u64 = 1200;
+const PULSE_NEUTRAL_US: u64 = 1500;
+const PULSE_MAX_US: u64 = 1800;
+
+fn default_map(position: i8) -> u64 {
+    return map_range(position, -100, 100, PULSE_MIN_US, PULSE_MAX_US);
+}
 
 pub struct ServoSixFront {
-    motor_front_left: SoftPwmPin<Gpio>,
-    motor_front_right: SoftPwmPin<Gpio>,
-    motor_back_left: SoftPwmPin<Gpio>,
-    motor_back_right: SoftPwmPin<Gpio>,
-    motor_front_turn: SoftPwmPin<Gpio>,
-    motor_back_thrust: SoftPwmPin<Gpio>,
-    map_range_in_min: i8,
-    map_range_in_max: i8,
-    map_range_out_min: i32,
-    map_range_out_max: i32,
+    motor_front_left: OutputPin,
+    motor_front_right: OutputPin,
+    motor_back_left: OutputPin,
+    motor_back_right: OutputPin,
+    motor_front_turn: OutputPin,
+    motor_back_thrust: OutputPin,
 }
 
 impl Motor for ServoSixFront {
     fn new() -> Self {
-        let pi = wiringpi::setup_gpio();
-
-        return ServoSixFront {
-            motor_front_left: pi.soft_pwm_pin(7),
-            motor_front_right: pi.soft_pwm_pin(0),
-            motor_back_left: pi.soft_pwm_pin(2),
-            motor_back_right: pi.soft_pwm_pin(3),
-            motor_front_turn: pi.soft_pwm_pin(21),
-            motor_back_thrust: pi.soft_pwm_pin(22),
-            map_range_out_min: 0,
-            map_range_out_max: 100,
-            map_range_in_min: -100,
-            map_range_in_max: 100,
+        let mut return_struct = ServoSixFront {
+            motor_front_left: Gpio::new().unwrap().get(7).unwrap().into_output(),
+            motor_front_right: Gpio::new().unwrap().get(11).unwrap().into_output(),
+            motor_back_left: Gpio::new().unwrap().get(13).unwrap().into_output(),
+            motor_back_right: Gpio::new().unwrap().get(15).unwrap().into_output(),
+            motor_front_turn: Gpio::new().unwrap().get(29).unwrap().into_output(),
+            motor_back_thrust: Gpio::new().unwrap().get(31).unwrap().into_output(),
         };
+
+        return_struct
+            .motor_front_left
+            .set_pwm(
+                Duration::from_millis(PERIOD_MS),
+                Duration::from_micros(PULSE_NEUTRAL_US),
+            )
+            .unwrap();
+        return_struct
+            .motor_front_right
+            .set_pwm(
+                Duration::from_millis(PERIOD_MS),
+                Duration::from_micros(PULSE_NEUTRAL_US),
+            )
+            .unwrap();
+        return_struct
+            .motor_back_left
+            .set_pwm(
+                Duration::from_millis(PERIOD_MS),
+                Duration::from_micros(PULSE_NEUTRAL_US),
+            )
+            .unwrap();
+        return_struct
+            .motor_back_right
+            .set_pwm(
+                Duration::from_millis(PERIOD_MS),
+                Duration::from_micros(PULSE_NEUTRAL_US),
+            )
+            .unwrap();
+        return_struct
+            .motor_front_turn
+            .set_pwm(
+                Duration::from_millis(PERIOD_MS),
+                Duration::from_micros(PULSE_NEUTRAL_US),
+            )
+            .unwrap();
+        return_struct
+            .motor_back_thrust
+            .set_pwm(
+                Duration::from_millis(PERIOD_MS),
+                Duration::from_micros(PULSE_NEUTRAL_US),
+            )
+            .unwrap();
+
+        return return_struct;
     }
 
     fn set_pitch(&mut self, motion: i8) {
         if motion == 0 {
-            self.motor_front_left.pwm_write(0);
-            self.motor_front_right.pwm_write(0);
-            self.motor_back_left.pwm_write(0);
-            self.motor_back_right.pwm_write(0);
+            self.motor_front_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(PULSE_NEUTRAL_US),
+                )
+                .unwrap();
+            self.motor_front_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(PULSE_NEUTRAL_US),
+                )
+                .unwrap();
+            self.motor_back_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(PULSE_NEUTRAL_US),
+                )
+                .unwrap();
+            self.motor_back_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(PULSE_NEUTRAL_US),
+                )
+                .unwrap();
         }
 
         if motion < 0 {
-            let mut set_range = map_range(
-                motion.clone(),
-                self.map_range_in_min.clone(),
-                self.map_range_in_max.clone(),
-                self.map_range_out_min.clone(),
-                self.map_range_out_max.clone(),
-            );
+            let mut set_range = default_map(motion.clone());
 
-            self.motor_front_left.pwm_write(set_range.clone());
-            self.motor_front_right.pwm_write(set_range.clone());
+            self.motor_front_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
+            self.motor_front_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
 
-            set_range = map_range(
-                motion.abs(),
-                self.map_range_in_min.clone(),
-                self.map_range_in_max.clone(),
-                self.map_range_out_min.clone(),
-                self.map_range_out_max.clone(),
-            );
+            set_range = default_map(motion.clone().abs());
 
-            self.motor_back_left.pwm_write(set_range);
-            self.motor_back_right.pwm_write(set_range);
+            self.motor_back_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
+            self.motor_back_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
         }
 
         if motion > 0 {
-            let mut set_range = map_range(
-                motion.clone(),
-                self.map_range_in_min.clone(),
-                self.map_range_in_max.clone(),
-                self.map_range_out_min.clone(),
-                self.map_range_out_max.clone(),
-            );
+            let mut set_range = default_map(motion.clone());
 
-            self.motor_front_left.pwm_write(set_range.clone());
-            self.motor_front_right.pwm_write(set_range.clone());
+            self.motor_front_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
+            self.motor_front_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
 
             let inverse_motion = motion - (motion * 2);
 
-            set_range = map_range(
-                inverse_motion,
-                self.map_range_in_min.clone(),
-                self.map_range_in_max.clone(),
-                self.map_range_out_min.clone(),
-                self.map_range_out_max.clone(),
-            );
+            set_range = default_map(inverse_motion);
 
-            self.motor_back_left.pwm_write(set_range);
-            self.motor_back_right.pwm_write(set_range);
+            self.motor_back_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
+            self.motor_back_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
         }
     }
 
     fn set_roll(&mut self, motion: i8) {
         if motion == 0 {
-            self.motor_front_left.pwm_write(0);
-            self.motor_front_right.pwm_write(0);
-            self.motor_back_left.pwm_write(0);
-            self.motor_back_right.pwm_write(0);
+            self.motor_front_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(PULSE_NEUTRAL_US),
+                )
+                .unwrap();
+            self.motor_front_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(PULSE_NEUTRAL_US),
+                )
+                .unwrap();
+            self.motor_back_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(PULSE_NEUTRAL_US),
+                )
+                .unwrap();
+            self.motor_back_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(PULSE_NEUTRAL_US),
+                )
+                .unwrap();
         }
 
         if motion < 0 {
-            let mut set_range = map_range(
-                motion.clone(),
-                self.map_range_in_min.clone(),
-                self.map_range_in_max.clone(),
-                self.map_range_out_min.clone(),
-                self.map_range_out_max.clone(),
-            );
+            let mut set_range = default_map(motion.clone());
 
-            self.motor_front_left.pwm_write(set_range.clone());
-            self.motor_back_left.pwm_write(set_range.clone());
+            self.motor_front_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
+            self.motor_back_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
 
-            set_range = map_range(
-                motion.abs(),
-                self.map_range_in_min.clone(),
-                self.map_range_in_max.clone(),
-                self.map_range_out_min.clone(),
-                self.map_range_out_max.clone(),
-            );
+            set_range = default_map(motion.clone().abs());
 
-            self.motor_front_right.pwm_write(set_range);
-            self.motor_back_right.pwm_write(set_range);
+            self.motor_front_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
+            self.motor_back_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
         }
 
         if motion > 0 {
-            let mut set_range = map_range(
-                motion.clone(),
-                self.map_range_in_min.clone(),
-                self.map_range_in_max.clone(),
-                self.map_range_out_min.clone(),
-                self.map_range_out_max.clone(),
-            );
+            let mut set_range = default_map(motion.clone());
 
-            self.motor_front_left.pwm_write(set_range.clone());
-            self.motor_back_left.pwm_write(set_range.clone());
+            self.motor_front_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
+            self.motor_back_left
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
 
             let inverse_motion = motion - (motion * 2);
 
-            set_range = map_range(
-                inverse_motion,
-                self.map_range_in_min.clone(),
-                self.map_range_in_max.clone(),
-                self.map_range_out_min.clone(),
-                self.map_range_out_max.clone(),
-            );
+            set_range = default_map(inverse_motion);
 
-            self.motor_front_right.pwm_write(set_range);
-            self.motor_back_right.pwm_write(set_range);
+            self.motor_front_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
+            self.motor_back_right
+                .set_pwm(
+                    Duration::from_millis(PERIOD_MS),
+                    Duration::from_micros(set_range.clone()),
+                )
+                .unwrap();
         }
     }
 
     fn set_yaw(&mut self, motion: i8) {
-        let mut set_range = map_range(
-            motion,
-            self.map_range_in_min.clone(),
-            self.map_range_in_max.clone(),
-            self.map_range_out_min.clone(),
-            self.map_range_out_max.clone(),
-        );
+        let set_range = default_map(motion.clone());
 
-        self.motor_front_turn.pwm_write(set_range);
+        self.motor_front_turn
+            .set_pwm(
+                Duration::from_millis(PERIOD_MS),
+                Duration::from_micros(set_range.clone()),
+            )
+            .unwrap();
     }
 
     fn set_acceleration(&mut self, motion: i8) {
-        let mut set_range = map_range(
-            motion,
-            self.map_range_in_min.clone(),
-            self.map_range_in_max.clone(),
-            self.map_range_out_min.clone(),
-            self.map_range_out_max.clone(),
-        );
+        let set_range = default_map(motion.clone());
 
-        self.motor_back_thrust.pwm_write(set_range);
+        self.motor_back_thrust
+            .set_pwm(
+                Duration::from_millis(PERIOD_MS),
+                Duration::from_micros(set_range.clone()),
+            )
+            .unwrap();
     }
 
     fn set_all(&mut self, acceleration: i8, yaw: i8, pitch: i8, roll: i8) {
